b9bdaf9f0d409380d8e4ed18923666fde1de4e50
cesar_campus_digital <cesar.villegas.cortes@gmail.com>
2026-06-09 10:20:11 -0400

    Se incorpora api con model viewSet y router register

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
